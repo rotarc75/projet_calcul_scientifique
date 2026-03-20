c86e994ed9e8992e296ada790813e9c0ab2d8bdf
Marc <suga75013@gmail.com>
2026-03-20 13:21:23 +0100

    getter fonctionnel !!

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -7,11 +7,11 @@ Triangle::Triangle(int n1, int n2, int n3){
 }
 
 
-int nonstatic get(int k){
+int Triangle::get(int k){
     switch (k){
         case 0 : return this->n1;
         case 1 : return this->n2;
         case 2 : return this-> n3;
-        case default : return -1;
+        default : return -1;
     }
 }
